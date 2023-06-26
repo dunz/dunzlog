2086f0f5de92fce8a2c25e99152579bccf326934
dunz <zergcake@gmail.com>
2023-06-26 18:27:27 +0900

    single quatation 백틱 으로 변경

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -12,7 +12,7 @@ const Profile: FC = () => {
         height={240}
         className="rounded-full"
       />
-      <p className="text-3xl font-black">Hi, I'm DongJu</p>
+      <p className="text-3xl font-black">Hi, I`m DongJu</p>
       <p className="text-xl font-black">Frontend Developer</p>
       사람들이 쉽게 접근할 수 있는 서비스를 만듭니다.
       <Link href="/contact" className="cursor-pointer rounded-full bg-amber-300 p-4 py-1 hover:opacity-80">
